b2f66703077bd15b543c44f298713db2d2588be1
DylanTrull <trulld@oregonstate.edu>
2021-06-05 17:38:28 -0700

    added more functions and test functions to calculator app.

--- test_calculator.py
+++ test_calculator.py
@@ -10,3 +10,9 @@ class TestCalculatorApp:
 
     def test_subtract(self):
         assert 5 == calculator.subtract(10, 5)
+
+    def test_division(self):
+        assert 5 == calculator.division(10, 2)
+
+    def test_multiplication(self):
+        assert 10 == calculator.multiplication(5, 2)
